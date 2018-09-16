0b6b16aa6860675dfbc67dc84894aa4e29a80d19
Lev Sivashov <lev@yoxel.com>
2018-09-16 12:23:21 +0300

    example of wrong highlighting

---
+++ src/main/scala/example/TailRec.scala
@@ -0,0 +1,28 @@
+package example
+
+import scala.annotation.tailrec
+
+trait TailRec[A] { self =>
+
+  def map[B](f: A => B): TailRec[B] = flatMap(f andThen (Return(_)))
+  def flatMap[B](f: A => TailRec[B]): TailRec[B] = FlatMap(this, f)
+}
+
+object TailRec {
+
+  @tailrec def run[A, B](io: TailRec[A]): A = io match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x, f) => x match {
+      case Return(a) => run(f(a))
+      case Suspend(r) => run(f(r()))
+      case FlatMap(y, g) => run(y flatMap (a => g(a) flatMap f))
+    }
+  }
+}
+
+case class Return[A](a: A) extends TailRec[A]
+case class Suspend[A](resume: () => A) extends TailRec[A]
+case class FlatMap[A, B](sub: TailRec[A], k: A => TailRec[B]) extends TailRec[B]
+
+
